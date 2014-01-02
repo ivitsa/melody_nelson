c9710d350f019ecb4c66787c963636f654e5e240
Ivica Salihbegovic <ivicasalihbegovic@gmail.com>
2014-01-02 22:48:25 +0100

    reshaping sizes in paperclip

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,11 @@
 class Event < ActiveRecord::Base
 	has_attached_file :image,
-		:styles => {:thumb => "100x100#", :medium => "x300"},
+		:styles => {:medium => "x140"},
 		:url => '/images/events/:id/:style/:basename.:extension',
 		:path => ':rails_root/public/images/events/:id/:style/:basename.:extension'
 		
-	
+	validates :headline, presence: true
+	validates :time, presence: true
 	validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png', 'image/gif']
 	validates_attachment_size :image, :less_than =>  5.megabytes
 end
